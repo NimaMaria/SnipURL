aabde096f477a7054a16a11bfdad26174934d8ab
NimaMaria <nimamariajacob@gmail.com>
2026-06-18 13:34:57 +0530

    ErrorHandler setup

---
+++ backend/controllers/authController.js
@@ -0,0 +1,18 @@
+const catchAsync = require('../middlewares/catchAsyncErrors');
+
+
+exports.signup = catchAsync(async(req,resizeBy,next) => {
+
+});
+
+exports.login = catchAsync(async(req,resizeBy,next) => {
+
+});
+
+exports.logout = catchAsync(async(req,resizeBy,next) => {
+
+});
+
+exports.forgotPassword = catchAsync(async(req,resizeBy,next) => {
+
+});
